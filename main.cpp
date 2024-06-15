623b6722801301053227e846675f3b3eb84ad3a1
William Dubuc <2243756@cegepoutaouais.qc.ca>
2024-06-15 13:34:10 -0400

    New comments

--- main.cpp
+++ main.cpp
@@ -10,13 +10,21 @@
 #include <cstring>
 #include "uart.h"
 
+/**
+ * Listen for new data on the uart and store it
+ */
 void uartListenerTask(UART* uart) {
     uart->listenForData();
 }
 
 void uartSenderTask(UART* uart) {
+    int count = 0;
     while (true) {
-        std::string message = "Hello";
+        std::string message = "Salut " + std::to_string(count);
+        count++;
+
+        std::cout << "Out: " << message << std::endl;
+
         uart->writeLine(message);
         uart->flush();
         std::this_thread::sleep_for(std::chrono::seconds(1));
@@ -24,23 +32,35 @@ void uartSenderTask(UART* uart) {
 }
 
 int main() {
+    //-------------------------------------------
+    //Variable declaration
     UART uart("/dev/ttyS0", B9600);
 
+    //-------------------------------------------
+    //Start threads
     std::thread listenerThread(uartListenerTask, &uart);
     std::thread senderThread(uartSenderTask, &uart);
 
+    //-------------------------------------------
+    //Main loop
     while (true) {
+        //-------------------------------------------
+        //Handle new data from the pico
         if (uart.isNewDataReceived()) {
             std::vector<std::string> lines = uart.getReceivedLines();
             for (const std::string& line : lines) {
-                std::cout << "Received: " << line << std::endl;
+                std::cout << "IN: " << line << std::endl;
                 // Additional processing logic for each line
             }
         }
+
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
 
+    //-------------------------------------------
+    //Wait for thread to finish
     listenerThread.join();
     senderThread.join();
+
     return 0;
 }
